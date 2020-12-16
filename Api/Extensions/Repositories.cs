95df3ec616c04e64fe2918e778297f8dd8b2f57a
Bruno Oliveira <bruninhopsk@outlook.com>
2020-12-15 21:00:00 -0300

    Configurado a injeção de dependência para acesso as interfaces dos repositorios

--- Api/Extensions/Repositories.cs
+++ Api/Extensions/Repositories.cs
@@ -1,4 +1,6 @@
+using Domain.Repositories;
 using Infrastructure.EntityFramework.Context;
+using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,8 @@ namespace Api.Extensions
         public static void AddRepositories(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AppDataContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
         }
     }
 }
