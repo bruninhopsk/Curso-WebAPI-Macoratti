15502783e9a894a9056b84adbb19073120f91edc
M. Oliveira Barbosa (c) <m.oliveira.barbosa@avanade.com>
2020-12-16 17:28:55 -0300

    Interface IUnitOfWork

---
+++ Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+namespace Domain.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository ProductRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+
+        void Commit();
+    }
+}
